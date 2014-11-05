4652e99e023c9a7e13f3dd6ccb82ff9017a8029f
William Hamilton <william@leroc.(none)>
2014-11-04 21:36:50 -0800

    Changed port setting

--- server.js
+++ server.js
@@ -95,8 +95,9 @@ app.get('*', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+app.set('port', (process.env.PORT || 5000));
 // run server!
-var server = app.listen(3000, function () {
+var server = app.listen(app.get('port'), function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('App listening @ http://%s:%s', host, port);
